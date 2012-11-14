f1582e573ef13ce35497cfece3bbedd7c8cc4e97
Ivan Fedorenkov <ivan.fedorenkov@gmail.com>
2012-11-14 09:51:39 -0800

    Modified representation of flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  @@flash_classes =
+    {:notice  => 'alert-info',
+     :alert   => 'alert-error',
+     :success => 'alert-success'}
+
+  def flash_class_for(key)
+    @@flash_classes[key]
+  end
 end
