2b804912397c097add14eec90efd4ce791aeb5a6
Ivan Fedorenkov <ivan.fedorenkov@gmail.com>
2012-11-14 06:21:03 -0800

    Initial creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Riado::Application.config.secret_token = 'daa96b55f71cd80439e9c4096eab1432f13be08da38de1952bf2100e3e380b1e13a8f8d30850b5bfdde88090e3d3ba07c0c989edcac7efebee8ba6fa2f3d8da2'
