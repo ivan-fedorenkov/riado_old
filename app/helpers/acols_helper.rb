138df4499d63916820adb4f14e52d3c215fb6429
Ivan Fedorenkov <ivan.fedorenkov@gmail.com>
2012-11-23 15:30:42 -0800

    fixed output of the acols helper

--- app/helpers/acols_helper.rb
+++ app/helpers/acols_helper.rb
@@ -2,14 +2,14 @@ module AcolsHelper
   
   def highlight_acol_prefix(acol_name)
     if (m = acol_name.match(/^(#{Acol.model_name.human})(.*)$/))
-      return "<strong>#{m[1]}</strong>#{m[2]}"
+      return ("<strong>#{m[1]}</strong>#{m[2]}").html_safe
     else
       return acol_name
     end
   end
   
   def acol_link_with_bolded_prefix(acol)
-    (link_to highlight_acol_prefix(acol.name), acol_path(acol)).html_safe
+    (link_to highlight_acol_prefix(acol.name), acol_path(acol).html_safe
   end
   
 end
